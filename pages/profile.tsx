498983f09bd2aa61fd7243d04a0f7d6d3ebc2e53
higoshi <h33je5sg@gmail.com>
2022-07-28 13:19:52 +0000

    init profile page

---
+++ pages/profile.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { NextPage } from 'next';
+
+const ProfilePage: NextPage = () => {
+  return <>
+    <h1>Profile Page</h1>
+  </>
+};
+
+export default ProfilePage;
