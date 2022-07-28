64b63e138924aa68d9a0a2aaaeee80150eec7e08
higoshi <h33je5sg@gmail.com>
2022-07-28 13:46:28 +0000

    add Demo Profile Page

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
 import { NextPage } from 'next';
+import { useUsername } from '@/lib/hooks/useUsername';
 
 const ProfilePage: NextPage = () => {
+  const {username, setUsername} = useUsername();
+  const [newUsername, setNewusername] = React.useState('');
+
   return <>
-    <h1>Profile Page</h1>
+    <h1>Demo Profile Page</h1>
+    <p>名前:{username}</p>
+    <hr/>
+    <label>
+      新しい名前を入力
+      <input
+        onChange={(e) => {
+          setNewusername(e.target.value);
+        }}
+      />
+    </label>
+    <button
+      onClick={() => {
+        setUsername(newUsername);
+      }}
+    >
+      送信
+    </button>
   </>
 };
 
