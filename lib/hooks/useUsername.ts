49414fd20bab95bcf76da0af40ac7d5513bc4ab9
higoshi <h33je5sg@gmail.com>
2022-07-28 13:46:18 +0000

    add useUsername

---
+++ lib/hooks/useUsername.ts
@@ -0,0 +1,21 @@
+import useSWR, { mutate } from 'swr';
+
+export const useUsername = () => {
+  const {
+    data,
+    error,
+    mutate,
+  } = useSWR<string>('/api/username', url => fetch(url).then(res => res.json()));
+
+  return {
+    username: data,
+    setUsername: async (newUsername: string) => {
+      await fetch('/api/username', {
+        method: 'POST',
+        body: JSON.stringify(newUsername),
+      });
+      mutate();
+    },
+    isLoading: !data && !error,
+  };
+};
